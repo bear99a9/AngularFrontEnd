5478cb40273f3bd90cc2f95d8b8dc543452532cf
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-03 11:46:27 +0100

    2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
