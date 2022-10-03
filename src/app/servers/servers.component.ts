1bc739fb24474fe2cd64cbe745f2d4ef7ee9548f
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-10-03 11:02:01 +0100

    One way databinding on event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server has been created';
+  serverName = "";
   constructor() { 
     setTimeout(() => {this.allowNewServer = true;}, 2000)
   }
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created'
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
   }
 
   ngOnInit(): void {
